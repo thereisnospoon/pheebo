b9e435790a9a9103204627338cf2fd3bfd9ce693
thereisnospoon <ocheyoba@gmail.com>
2014-08-19 23:43:08 +0300

    Minor changes and fixes

--- src/main/java/my/thereisnospoon/pheebo/controllers/HomeController.java
+++ src/main/java/my/thereisnospoon/pheebo/controllers/HomeController.java
@@ -1,29 +1,19 @@
 package my.thereisnospoon.pheebo.controllers;
 
-import my.thereisnospoon.pheebo.persistance.model.Board;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
 	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	@RequestMapping(value = "/rest/{board}", method = RequestMethod.GET, headers = {"Accept=application/json"})
-	@ResponseBody
-	public String rest(@PathVariable String board) {
-
-		log.debug("board " + board);
-
-		Board boardEntity = entityManager.find(Board.class, board);
-		return boardEntity != null ? boardEntity.toString() : "null";
+	@RequestMapping(method = RequestMethod.GET)
+	public String home() {
+		return "index";
 	}
 }
